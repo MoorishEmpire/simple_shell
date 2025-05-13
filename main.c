3b2a753369e4d93455ae40b5ec1a00de29545183
Abdelkarim Elmostakim <abdelkarimelmostakim07@gmail.com>
2025-05-13 11:56:55 -0700

    shell 0.1

--- main.c
+++ main.c
@@ -25,89 +25,140 @@ char *read_command(void)
 
 int count_tokens(char *cmd)
 {
+	int count = 0;
 	char *copy = strdup(cmd);
 	char *token = strtok(copy, DELIM);
-	int count = token ? 1 : 0;
 
+	while (token)
+	{
+		count++;
+		token = strtok(NULL, DELIM);
+	}
 	free(copy);
 	return (count);
 }
 
-char **build_argv(char *cmd, int argc)
+char *resolve_path(char *cmd)
 {
-	char **argv;
-	char *token;
-	int i = 0;
+    char *path = getenv("PATH");
+    char *path_copy = strdup(path);
+    char *dir = strtok(path_copy, ":");
+    char *full_path = NULL;
 
-	(void)argc;
-	argv = malloc(sizeof(char *) * 2);
-	if (!argv)
-		return (NULL);
-	token = strtok(cmd, DELIM);
-	argv[0] = token;
-	argv[i] = NULL;
-	return (argv);
+    if (strchr(cmd, '/')) {
+        if (access(cmd, X_OK) == 0) {
+            full_path = strdup(cmd);
+            free(path_copy);
+            return full_path;
+        }
+        free(path_copy);
+        return NULL;
+    }
+    while (dir) {
+        full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+        sprintf(full_path, "%s/%s", dir, cmd);
+        if (access(full_path, X_OK) == 0) {
+            free(path_copy);
+            return full_path;
+        }
+        free(full_path);
+        dir = strtok(NULL, ":");
+    }
+    free(path_copy);
+    return NULL;
+}
+
+char **build_argv(char *cmd)
+{
+    char **argv = NULL;
+    char *token;
+    int i = 0;
+    char *cmd_copy = strdup(cmd);
+
+    int count = 0;
+    token = strtok(cmd_copy, DELIM);
+    while (token)
+    {
+        count++;
+        token = strtok(NULL, DELIM);
+    }
+    free(cmd_copy);
+    argv = malloc(sizeof(char *) * (count + 1));
+    if (!argv)
+        return NULL;
+    cmd_copy = strdup(cmd);
+    token = strtok(cmd_copy, DELIM);
+    while (token)
+    {
+        argv[i++] = strdup(token);
+        token = strtok(NULL, DELIM);
+    }
+    argv[i] = NULL;
+    free(cmd_copy);
+    return argv;
 }
 
 void execute_command(char **argv)
 {
-	int pid = fork();
-	int status;
+    char *full_path;
+    int pid;
+    int status;
 
-	if (pid < 0)
-	{
-		perror("fork");
-		return;
-	}
-	if (pid == 0)
-	{
-		if (execve(argv[0], argv, environ) == -1)
-		{
-			fprintf(stderr, "./shell: No such file or directory\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-		wait(&status);
+    full_path = resolve_path(argv[0]);
+    if (!full_path)
+    {
+        fprintf(stderr, "%s: command not found\n", argv[0]);
+        return;
+    }
+    pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        free(full_path);
+        return;
+    }
+    if (pid == 0) {
+        if (execve(full_path, argv, environ) == -1) {
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        wait(&status);
+        free(full_path);
+    }
 }
 
 int main(void)
 {
-	char *cmd;
-	int argc;
-	char **argv;
+    char *cmd;
+    char **argv;
+    int is_interactive = isatty(STDIN_FILENO);
+    int	i;
 
-	while (1)
+    while (1)
+    {
+        if (is_interactive)
+            print_prompt();
+        cmd = read_command();
+        if (!cmd)
+            break;
+        if (strcmp(cmd, "exit\n") == 0) {
+            free(cmd);
+            break;
+        }
+        argv = build_argv(cmd);
+        if (!argv) {
+            free(cmd);
+            continue;
+        }
+        execute_command(argv);
+	i = 0;
+	while (argv[i])
 	{
-		print_prompt();
-		cmd = read_command();
-		if (!cmd)
-			break;
-		if(strcmp(cmd, "exit\n") == 0)
-		{
-			free(cmd);
-			break;
-		}
-		argc = count_tokens(cmd);
-		if (argc == 0)
-		{
-			free(cmd);
-			continue;
-		}
-
-		argv = build_argv(cmd, argc);
-		if (!argv)
-		{
-			free(cmd);
-			continue;
-		}
-
-		execute_command(argv);
-
-		free(argv);
-		free(cmd);
+		free(argv[i]);
+		i++;
 	}
-
-	return (0);
+        free(argv);
+        free(cmd);
+    }
+    return 0;
 }
-
